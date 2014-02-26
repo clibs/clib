dd5ee0d388d0f95f4d16c9d2561541e6f7c2d81e
Stephen Mathieson <smath23@gmail.com>
2014-02-26 15:55:48 -0500

    test: Ensure we've put the executable on PATH

--- test/install-binary-dependencies.sh
+++ test/install-binary-dependencies.sh
@@ -5,3 +5,7 @@ clib install stephenmathieson/tabs-to-spaces@1.0.0 > /dev/null
   echo >&2 "Failed to install stephenmathieson/tabs-to-spaces";
   exit 1
 }
+command -v t2s >/dev/null 2>&1 || {
+  echo >&2 "Failed to put t2s on path";
+  exit 1
+}
