1c94f8f81635fdf8c1485aa8090d771201cfed48
Stephen Mathieson <smath23@gmail.com>
2014-06-29 16:14:01 -0400

    test: Add two basic testcases for clib-search(1)

---
+++ test/search-basic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CACHE=$TMPDIR/clib-search.cache
+rm -f $CACHE 2> /dev/null
+
+N=`clib search | wc -l`
+# lame check for more than 100 lines of output
+[ $N -lt 100 ] && {
+  echo >&2 "Expected `clib search` to return at least 100 results";
+  exit 1
+}
+
+TRIM=`clib search trim`
+[[ "$TRIM" == *"stephenmathieson/trim.c"* ]] || {
+  echo >&2 "Expected \`clib search trim\` to output stephenmathieson/trim.c";
+  exit 1
+}
