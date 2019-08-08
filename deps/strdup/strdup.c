d7fb89205a8fea8675903aadc012c91a36dfddf6
Joseph Werle <joseph.werle@gmail.com>
2019-08-08 13:19:15 -0400

    chore(deps/): Update 'strdup'

--- deps/strdup/strdup.c
+++ deps/strdup/strdup.c
@@ -14,9 +14,17 @@
 
 char *
 strdup(const char *str) {
+  if (NULL == (char *) str) {
+    return NULL;
+  }
+
   int len = strlen(str) + 1;
   char *buf = malloc(len);
-  if (buf) memcpy(buf, str, len);
+
+  if (buf) {
+    memset(buf, 0, len);
+    memcpy(buf, str, len - 1);
+  }
   return buf;
 }
 
