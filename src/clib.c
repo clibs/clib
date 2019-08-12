38b8ad8475f9aafb45c0ff1434cfa37cbd9909a8
Joseph Werle <joseph.werle@gmail.com>
2019-08-12 17:50:16 -0400

    refactor(src/clib.c): Add 'configure' command

--- src/clib.c
+++ src/clib.c
@@ -30,10 +30,11 @@ static const char *usage =
   "\n"
   "  Commands:\n"
   "\n"
-  "    init               Start a new project\n"
-  "    install [name...]  Install one or more packages\n"
-  "    search [query]     Search for packages\n"
-  "    help <cmd>         Display help for cmd\n"
+  "    init                 Start a new project\n"
+  "    install [name...]    Install one or more packages\n"
+  "    configure [name...]  Configure one or more packages\n"
+  "    search [query]       Search for packages\n"
+  "    help <cmd>           Display help for cmd\n"
   "";
 
 #define format(...) ({                               \
