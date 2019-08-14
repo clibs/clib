f1c01fb70efbfd09fbcd64ef1f214e512c8741a0
Joseph Werle <joseph.werle@gmail.com>
2019-08-14 13:25:52 -0400

    refactor(): automate 'version.h'

---
+++ index.js
@@ -0,0 +1,8 @@
+const { resolve } = require('path')
+exports.paths = {
+  'clib': resolve(__dirname, 'clib'),
+  'clib-install': resolve(__dirname, 'clib-install'),
+  'clib-configure': resolve(__dirname, 'clib-configure'),
+  'clib-init': resolve(__dirname, 'clib-init'),
+  'clib-search': resolve(__dirname, 'clib-search'),
+}
