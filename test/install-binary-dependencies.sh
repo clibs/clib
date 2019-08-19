23f64551ec0a42b7e7763ebbf88280669006178c
Joseph Werle <joseph.werle@gmail.com>
2019-08-19 15:17:22 -0400

    fix(test/install-binary-dependencies.sh): use 't2s' in $PREFIX/bin path

--- test/install-binary-dependencies.sh
+++ test/install-binary-dependencies.sh
@@ -11,7 +11,7 @@ clib install --skip-cache stephenmathieson/tabs-to-spaces@1.0.0 > /dev/null || {
   echo >&2 "Failed to install stephenmathieson/tabs-to-spaces"
   exit 1
 }
-command -v t2s >/dev/null 2>&1 || {
+command -v $PREFIX/bin/t2s >/dev/null 2>&1 || {
   echo >&2 "Failed to put t2s on path"
   exit 1
 }
