a48425d94dbdb2f064c2955a7b18d6fe0a647c44
Isty001 <isty001@gmail.com>
2020-09-08 16:13:00 +0200

    fix: clib-package.c leaks

--- src/common/clib-package.c
+++ src/common/clib-package.c
@@ -657,10 +657,8 @@ clib_package_new_from_slug_with_package_name(const char *slug, int verbose,
 #ifdef HAVE_PTHREADS
       init_curl_share();
       _debug("GET %s", json_url);
-      if (res) {
-        // clean up when retrying
-        http_get_free(res);
-      }
+      // clean up when retrying
+      http_get_free(res);
       res = http_get_shared(json_url, clib_package_curl_share);
 #else
       res = http_get(json_url);
