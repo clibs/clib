91c8c64423828e02cdb8065ac496b7e88bd163c5
AdamKorcz <adam@adalogics.com>
2020-11-14 16:28:00 +0000

    Added fuzzer

---
+++ test/fuzzing/fuzz_manifest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "../../src/common/clib-package.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    if(size<3){
+            return 0;
+    }
+    char filename[256];
+    sprintf(filename, "libfuzzer.json");
+
+    FILE *fp = fopen(filename, "wb");
+    if (!fp)
+            return 0;
+    fwrite(data, size, 1, fp);
+    fclose(fp);
+    
+    clib_package_load_from_manifest(filename, 0);
+    
+    unlink(filename);
+    return 0;
+}
