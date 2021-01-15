bc95ba00858c7d8fb3f1224646353a30300db6f5
Isty001 <isty001@gmail.com>
2021-01-15 18:06:31 +0100

    clib init can't always determine the current directory name (#240)
    * clib init can't always determine the current directory name
    
    * clib init can't always determine the current directory name

--- src/clib-init.c
+++ src/clib-init.c
@@ -54,12 +54,8 @@ static void setopt_manifest_file(command_t *self) {
 static char *find_basepath() {
   char cwd[4096] = {0};
   getcwd(cwd, 4096);
-  char *walk = cwd + strlen(cwd);
-  while (*(--walk) != '/')
-    ;
-  char *basepath = malloc((size_t)(walk - cwd));
-  strncpy(basepath, walk + 1, (size_t)(walk - cwd));
-  return basepath;
+
+  return strdup(strrchr(cwd, '/') + 1);
 }
 
 static void getinput(char *buffer, size_t s) {
