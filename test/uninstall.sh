7cda5b9dfacc5a6789e56a32a6294df36745bcf9
Farkasvölgyi <isty001@gmail.com>
2021-01-22 10:19:01 +0100

    Intergrating clib-uninstall

--- test/uninstall.sh
+++ test/uninstall.sh
@@ -5,7 +5,7 @@ clib install -c stephenmathieson/tabs-to-spaces@1.0.0 -P tmp > /dev/null || {
   exit 1
 }
 
-PREFIX=/tmp ./clib-uninstall stephenmathieson/tabs-to-spaces
+PREFIX=tmp ./clib-uninstall stephenmathieson/tabs-to-spaces
 
 # ensure the un-installation worked
 command -v tmp/bin/t2s >/dev/null 2>&1 && {
