58b1f98ac69fd994121b313bd1cda8c9abe81717
Farkasvölgyi <isty001@gmail.com>
2022-03-22 09:46:34 +0100

    swap tests

--- test.sh
+++ test.sh
@@ -2,24 +2,7 @@
 
 export DEBUG=*
 
-
-mkdir -p tmp/bin
-TESTS=$(find test/* -type f -perm -111)
 EXIT_CODE=0
-export PATH="$PWD:$PATH"
-
-printf "\nRunning clib(1) tests\n\n"
-
-for t in $TESTS; do
-  if ! ./"$t"; then
-    echo >&2 "  (✖) $t"
-    EXIT_CODE=1
-  else
-    echo "  (✓) $t"
-  fi
-done
-echo
-
 
 printf "\nRunning clib package tests\n\n"
 cd test/package && make clean
@@ -38,5 +21,22 @@ if ! make test; then
 fi
 
 cd ../../
+
+
+mkdir -p tmp/bin
+TESTS=$(find test/* -type f -perm -111)
+export PATH="$PWD:$PATH"
+
+printf "\nRunning clib(1) tests\n\n"
+
+for t in $TESTS; do
+  if ! ./"$t"; then
+    echo >&2 "  (✖) $t"
+    EXIT_CODE=1
+  else
+    echo "  (✓) $t"
+  fi
+done
+echo
 
 exit $EXIT_CODE
