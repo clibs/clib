9316446271b75015e5c517ab43a00112f66a6209
maflcko <6399679+maflcko@users.noreply.github.com>
2024-03-29 11:03:16 +0100

    Update fuzz_manifest.c: Add missing include unistd.h

--- test/fuzzing/fuzz_manifest.c
+++ test/fuzzing/fuzz_manifest.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "../../src/common/clib-package.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
